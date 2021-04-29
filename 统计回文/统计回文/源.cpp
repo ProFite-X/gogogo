82648b04dd0e8c4bff3ed9e88faa313a47336f37
ProFite-X <65878288+ProFite-X@users.noreply.github.com>
2021-04-29 23:01:08 +0800

    Add files via upload

---
+++ 统计回文/统计回文/源.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	cin >> n;
+
+	vector<int> arr;
+	for (int i = 0; i < n; i++)
+	{
+		cin>>arr[i];
+	}
+	return 0;
+	
+}
